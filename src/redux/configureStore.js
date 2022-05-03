e76c26d8f769f3ebd6237ecad7ff6c1760bc817e
fed1k <allamurotovcoder@mail.ru>
2022-05-03 05:34:05 +0500

    Fix book reducer to add and remove

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,9 @@
 import { createStore, combineReducers } from 'react-redux';
 import reducerForCategory, { createActionForCategory } from './categories/categories';
-import reducer, { createActionForBook } from './books/books';
+import reducer, { createActionForBookToAdd } from './books/books';
 
 const rootReducer = combineReducers({ books: reducer, categories: reducerForCategory });
 
 const store = createStore(rootReducer);
 store.dispatch(createActionForCategory());
-store.dispatch(createActionForBook());
+store.dispatch(createActionForBookToAdd());
