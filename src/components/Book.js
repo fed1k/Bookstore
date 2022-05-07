f779498ff3dddd784b89a82a7451467fef5ea062
fed1k <allamurotovcoder@mail.ru>
2022-05-07 11:16:12 +0500

    Finish the whole styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -11,24 +11,48 @@ const Book = ({
     dispatch({ type: 'REMOVE_BOOK', payload: id });
   };
   const [changeableId, setChangeableId] = useState(id);
-
+  const [progress, setProgress] = useState('UPDATE PROGRESS');
+  const [progressClass, setProgressClass] = useState('update-progress-btn');
   return (
     <div className="book">
-      <p className="category-p">{category}</p>
-      <h1>{title}</h1>
-      <p>{author}</p>
-      <button
-        type="button"
-        onClick={() => {
-          dispatcher();
-          setChangeableId(id);
-          localStorage.setItem('id', JSON.stringify({ idkart: changeableId }));
-          dispatch(deleteDataFromApi());
-        }}
-      >
-        Remove
+      <div>
+        <p className="category-p">{category}</p>
+        <h1>{title}</h1>
+        <p className="author">{author}</p>
+        <div>
+          <button type="button" className="comments">Comments</button>
+          <button
+            className="remove"
+            type="button"
+            onClick={() => {
+              dispatcher();
+              setChangeableId(id);
+              localStorage.setItem('id', JSON.stringify({ idkart: changeableId }));
+              dispatch(deleteDataFromApi());
+            }}
+          >
+            Remove
 
-      </button>
+          </button>
+          <button type="button" className="edit">Edit</button>
+        </div>
+      </div>
+      <div className="chapter-div">
+        <p>CURRENT CHAPTER</p>
+        <p>Chapter 3 A lesson learned</p>
+        <button
+          type="button"
+          className={progressClass}
+          onClick={() => {
+            setProgress('UPDATED');
+            setProgressClass('update-progress-btn-completed');
+          }}
+        >
+          {progress}
+
+        </button>
+      </div>
+
     </div>
   );
 };
